415d6acb67d195e1f4afdaea13aef6e5a248dfe3
Halden <haldenk@razoyo.com>
2026-04-09 14:22:45 -0600

    fix: plugin-deactivation Do not clean up any cridentials/webhooks on deactivate or uninstall

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__ . '/src')
     ->in(__DIR__ . '/tests')
-    ->append([__DIR__ . '/kenzi-chat.php', __DIR__ . '/uninstall.php']);
+    ->append([__DIR__ . '/kenzi-chat.php']);
 
 return (new PhpCsFixer\Config())
     ->setRules([
--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -22,7 +22,7 @@ mkdir -p "$DEST"
 
 # Copy production files
 cp -r src assets "$DEST/"
-cp kenzi-chat.php uninstall.php readme.txt LICENSE composer.json "$DEST/"
+cp kenzi-chat.php readme.txt LICENSE composer.json "$DEST/"
 
 # Install production autoloader (no dev dependencies)
 composer install --no-dev --optimize-autoloader --no-interaction --quiet --working-dir="$DEST"
